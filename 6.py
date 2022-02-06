9535cfbebf2afe3c68a1b4aeedc87b53f2b4ac9d
murmankot <int1312art1312>
2022-02-06 19:27:57 +0300

    Добавлен ответ на задание 6

---
+++ 6.py
@@ -0,0 +1,10 @@
+import requests
+
+response = requests.get('https://playground.learnqa.ru/api/long_redirect', allow_redirects=True)
+
+list_of_redirects = response.history
+number_of_redirects = len(list_of_redirects)
+print(number_of_redirects)
+
+final_response = response
+print(final_response.url)
